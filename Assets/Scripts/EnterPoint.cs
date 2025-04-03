b2e38f88c46f31b04e29222d800eb7528ce618ed
Sveta <svetamashenko@yandex.ru>
2025-04-03 18:03:42 +0300

    Comment
    Comments added

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,7 +11,13 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
-    
+
+    // Эт коммент
+
+    /*
+    Эт большой коммент
+    */
+
     void Start()
     {
         Time.timeScale = _timeScale;
